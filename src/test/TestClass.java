f775faa403db8377cdb76ce159bd23b3f39b7640
nemus92 <nemusdragin@gmail.com>
2016-03-08 09:37:39 +0100

    This is a commit message from Nemanja @T127

--- src/test/TestClass.java
+++ src/test/TestClass.java
@@ -2,6 +2,6 @@ package test;
 
 public class TestClass {
 	public static void main(String[] args) {
-		System.out.println("Hello world blaaaaa");
+		System.out.println("Hello world blaaaaaaa");
         }
 }
